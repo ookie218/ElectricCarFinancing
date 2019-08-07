2f7e302d2c1cff9752bf2338235d947d16c4e419
Ookie Ebron <ookieebron@gmail.com>
2019-08-07 13:17:14 -0400

    Initial commit

---
+++ app/src/main/java/com/ookie/electriccarfinancing/MainActivity.java
@@ -0,0 +1,47 @@
+package com.ookie.electriccarfinancing;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    int intYears;
+    int intLoan;
+    float decInterest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText years = findViewById(R.id.txtYears);
+        final EditText loan = findViewById(R.id.txtLoan);
+        final EditText interest = findViewById(R.id.txtInterest);
+
+        Button button = findViewById(R.id.btnPayment);
+
+        final SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intYears = Integer.parseInt(years.getText().toString());
+                intLoan = Integer.parseInt(loan.getText().toString());
+                decInterest = Float.parseFloat(interest.getText().toString());
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putInt("key1", intYears);
+                editor.putInt("key2", intLoan);
+                editor.putFloat("key3", decInterest);
+                editor.commit();
+                startActivity(new Intent(MainActivity.this, Payment.class));
+            }
+        });
+
+    }
+}
